f72c9748f727dff305e2ac5e80224aea794f9ba4
TI Sabbir <tisabbir0@gmail.com>
2024-07-03 21:47:35 +0600

    update: footer

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Pages/Home/Navbar/Navbar";
+import Footer from "../Pages/Home/Footer/Footer";
 
 const Root = () => {
   return (
     <div>
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
